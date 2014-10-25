a25a2bd79b78874ae22fbdccc38b147359946b6b
Chun-Kwan Alex Fung <cfung58@uwo.ca>
2014-10-25 00:07:27 -0400

    Final working version commit

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,7 +1,10 @@
-/*
-// tweetslist data array for filling in info box
-var tweetListData = [];
-*/
+
+// tweetslist data array and //dateListData for containing the different tweet and dates 
+//assumption: both arrays will have the same size i 
+var tweetString = ""; 
+var tweetJSON; 
+var dateString = ""; 
+var dateJSON; 
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -9,9 +12,10 @@ $(document).ready(function() {
     // Populate the user table on initial page load
     populateTable();
 
-    // Add User button click
+    // Add User button click, on click it will run function addTweet located below 
     $('#btnAddTweet').on('click', addTweet);
 
+	
 });
 
 // Functions =============================================================
@@ -26,12 +30,13 @@ function populateTable() {
     $.getJSON( '/tweets/tweetlist', function( data ) {
 
 	
-        // every item in our JSON is addded  to a table row and their specific cells to the content string
+        // every item in our JSON is added  to a table row and their specific cells to the content string
         $.each(data, function(){
 	    //first we must conver the this.date from ISOString back to Date object in javascript, so it's easily visible
 	    var dateObject = new Date(this.date);
 
             tableContent += '<tr>';
+	    tableContent += '<td id="'+ this.tweet +'">' + this._id + '</td>';
             tableContent += '<td>' + this.tweet + '</td>';
             tableContent += '<td>' + dateObject + '</td>';
             tableContent += '</tr>';
@@ -56,9 +61,9 @@ function addTweet(event) {
     
     // Makes sure that there is actually input in the tweet box 
     $('#addTweet input').each(function(index, val) {
-        if($(this).val() === '') { 
+        if($(this).val() === '' || $(this).val() === '#') { 
 		noInput = true; 
-		alert("Please fill in something in the hashtag input");  }
+		alert("Please fill in something in the hashtag input (input cannot be just hashtag as well)");  }
     });	
 
     if (noInput === true){
@@ -100,13 +105,104 @@ function addTweet(event) {
 	//MongoDB only accepts dates in ISO string, so we must conver it to ISO String 
 	var ISOdate = d.toISOString(); 
 
-        // If it is, compile all user info into one object
-        var newTweet = {
-            'tweet': $('#addTweet fieldset input#inputTweetName').val(),
-            'date': ISOdate 
-        }
+	//create variables to be added later to local array 
+	tweetString =  $('#addTweet fieldset input#inputTweetName').val().toString(); 
+	tweetJSON = $('#addTweet fieldset input#inputTweetName').val(); 
+	dateString = d.toString();
+	dateJSON =ISOdate; 
+	//alert ("tweetString: " + tweetString + ", dateString: " + dateString);  
 
-        // Use AJAX to post the object to our addtweet service
+	//check database for similarities
+	checkDatabase(); 
+	
+    }
+    
+};
+
+//checks the database first to see if there are any existing hashtags with the same hashtag name 
+function checkDatabase(){
+    // jQuery AJAX call for JSON
+    //alert("running!!!"); 
+
+    $.getJSON( '/tweets/alltweets', function( data ) {
+
+	
+        // every item in our JSON is added  to a table row and their specific cells to the content string
+        $.each(data, function(){
+	    //first we must convert the this.date from ISOString back to Date object in javascript, so it's easily visible
+	    var eachDate = new Date(this.date);
+	    
+	    //same thing with tweet name 
+	    var eachTweet = this.tweet;
+	    
+	    //alert("eachTweet from alltweets: " + eachTweet + "  , tweetString: " + tweetString); 
+	    
+ 
+	    //compare eachTweet with tweetString 
+	    if (eachTweet === tweetString){
+		//alert("tweet comparison found!");
+
+		//get the id 
+		//TODO: figure out a way to get ID 
+		var id = this._id; 
+		//alert ("id of the found tweet: " + id); 
+		deleteTweet(id);  
+		//var id = document.getElementById(tweetString).childNodes[0].nodeValue; 	
+		//alert ("id: " + id);
+		//alert ("o_id: " + o_id); 
+
+	    }
+		
+	    	
+            
+        });
+
+	//at the end we call the AJAXAddTweet() to add the tweet 
+	AJAXAddTweet(); 
+
+    });
+
+}; 
+
+
+function deleteTweet(o_id){
+		//call for DELETE
+		// Use AJAX to delete the object from our addtweet service 
+		//alert ("we are here with in the final mode" ); 
+
+        	$.ajax({
+            		type: 'DELETE',
+            		url: '/tweets/deletetweet/' + o_id
+        	}).done(function( response ) {
+
+            		// Check for successful (blank) response
+            		if (response.msg === '') {
+
+                		// Clear the form inputs so the user doesn't have to backspace them everytime 
+                		$('#addTweet fieldset input').val('');
+				 
+                		// Update the table                
+				populateTable(); 
+
+            		}
+           		 else {
+
+                		// If something goes wrong, alert the error message that our service returned
+               			 alert('Error: ' + response.msg);
+
+            		}
+        	});
+
+}; 
+
+
+function AJAXAddTweet(){
+	var newTweet = {
+		'tweet': tweetJSON, 
+		'date': dateJSON
+	}	
+
+	// Use AJAX to post the object to our addtweet service
         $.ajax({
             type: 'POST',
             data: newTweet,
@@ -119,9 +215,10 @@ function addTweet(event) {
 
                 // Clear the form inputs so the user doesn't have to backspace them everytime 
                 $('#addTweet fieldset input').val('');
-
-                // Update the table
-                populateTable();
+		
+		//alert("table about to be populated");  
+                // Update the table                
+		populateTable(); 
 
             }
             else {
@@ -131,6 +228,10 @@ function addTweet(event) {
 
             }
         });
-    }
-    
-};
+
+
+}; 
+
+
+
+
--- routes/tweets.js
+++ routes/tweets.js
@@ -2,24 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /*
- * GET tweetlist.
- 
-router.get('/tweetlist', function(req, res) {
+ * GET ALL THE TWEEEEEEETS
+*/ 
+router.get('/alltweets', function(req, res) {
     var db = req.db;
-    db.collection('tweetlist').find().toArray(function (err, items) {
+    db.collection('tweetdb').find().toArray(function (err, items) {
         res.json(items);
     });
 });
-*/
-
 
 
 /*
- * POST to addtweet. POST will also check the database to see if there is already the same 
+ * POST to addtweet.  
  */
 router.post('/addtweet', function(req, res) {
     var db = req.db;
-    db.collection('tweetlist').insert(req.body, function(err, result){
+   
+    db.collection('tweetdb').insert(req.body, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
@@ -30,11 +29,22 @@ router.post('/addtweet', function(req, res) {
 //NEW GET that also sorts the database by date (latest ten tweets will go first) before delivering it to the js  
 router.get('/tweetlist', function(req, res) {
     var db = req.db;
-    db.collection('tweetlist').find().sort({$natural:-1}).limit(10).toArray(function (err, items) {
+    db.collection('tweetdb').find().sort({$natural:-1}).limit(10).toArray(function (err, items) {
 	res.json(items);
 	});
 });
 
+
+/*
+ * DELETE to deletetweet
+ */
+router.delete('/deletetweet/:id', function(req, res) { 
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('tweetdb').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:' delete tweet error: ' + err });
+    });
+});
 
 
 module.exports = router;
